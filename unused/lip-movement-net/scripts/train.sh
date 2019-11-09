93521c8c6b4fe14c3e5aa1323ef7b9dc2f09c8a8
LewiVir <lewivir@163.com>
2019-11-09 15:25:41 +0800

    add Speaker-Diarization

--- unused/lip-movement-net/scripts/train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clear
-python ../src/lip_movement_net.py -i ../data/dataset_source
--- unused/lip-movement-net/scripts/train_using_grid_search.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clear
-python ../src/lip_movement_net.py -go ../csv/grid_options.csv -gr ../csv/grid_results.csv -i ../data/dataset_source
